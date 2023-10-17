47992b12217c04a2a06b5dc211c596ad61095029
Kha <khanguyen1204@gmail.com>
2023-10-17 11:09:36 +0100

    feat: add basic features

--- src/qlsv/QLSV.java
+++ src/qlsv/QLSV.java
@@ -4,6 +4,10 @@
  */
 package qlsv;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.JDBC;
+
 /**
  *
  * @author A715-42G
@@ -15,7 +19,26 @@ public class QLSV {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("");
+                JDBC dbCon = new JDBC();
+         try {
+             String query = "SELECT * FROM dbo.SINHVIEN";
+             ResultSet rs = dbCon.executeQuery(query);
+             while(rs.next()){
+                 String msSV = rs.getString("MSSV");
+                 String tenSV = rs.getString("TENSV");
+                 String soDT = rs.getString("SODT");
+                 String lop = rs.getString("LOP");
+                 String diaChi = rs.getString("DIACHI");
+                 System.out.println(msSV);
+                 System.out.println(tenSV);
+                 System.out.println(soDT);
+                 System.out.println(lop);
+                 System.out.println(diaChi);
+                  System.out.println("");
+             }
+         }
+         catch(SQLException e){
+         }
     }
     
 }
